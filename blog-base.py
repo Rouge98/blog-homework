e3e5ddc7c887e32fbe6a96e66147638f972938e7
maddox-johnson <144567126+Rouge98@users.noreply.github.com>
2026-03-01 15:57:34 -0500

    Add files via upload

---
+++ blog-base.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Django Blog</title>
+</head>
+<body>
+    <h1><a href="/">My Blog</a></h1>
+    <hr>
+
+    {% block content %}
+    {% endblock %}
+
+</body>
+</html>
